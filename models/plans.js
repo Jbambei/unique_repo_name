2b5871994bf7b0e877216632613ac9c5ffb8f678
seanRudd <seanmrudd@me.com>
2019-06-25 21:34:30 -0400

    eslint fixes

--- models/plans.js
+++ models/plans.js
@@ -1,31 +1,31 @@
 /* eslint-disable camelcase */
 module.exports = function(sequelize, DataTypes) {
-    var Plans = sequelize.define("Plans", {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      plan_title: {
-        type: DataTypes.STRING,
-        required: true
-      },
-      plan_author: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      plan_title: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      plan_content: {
-          type: DataTypes.STRING,
-          required: true
-      },
-      scenario_created_at: {
-        type: DataTypes.DATE,
-        allowNull: false
-      }
-    });
-    return Plans;
-  };
+  var Plans = sequelize.define("Plans", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    plan_title: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    plan_author: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    plan_title: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    plan_content: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    scenario_created_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
+  return Plans;
+};
--- models/response.js
+++ models/response.js
@@ -1,22 +1,23 @@
+/* eslint-disable camelcase */
 module.exports = function(sequelize, DataTypes) {
-    var Response = sequelize.define("Plans", {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      response_author: {
-        type: DataTypes.STRING,
-        required: true
-      },
-      response_content: {
-          type: DataTypes.TEXT,
-          required: true
-      },
-      scenario_created_at: {
-        type: DataTypes.DATE,
-        allowNull: false
-      }
-    });
-    return Response;
-  };
+  var Response = sequelize.define("Plans", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    response_author: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    response_content: {
+      type: DataTypes.TEXT,
+      required: true
+    },
+    scenario_created_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
+  return Response;
+};
