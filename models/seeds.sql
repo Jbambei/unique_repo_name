37d41e52a57b62e1a85b96e07243e6b5aba9176a
Joel Bambei <joelbambei@gmail.com>
2019-06-27 20:16:26 -0400

    created seeds

---
+++ models/seeds.sql
@@ -0,0 +1,35 @@
+ --  Scenario seeds 
+INSERT INTO 'Scenario' ('scenario_title', 'scenario_author', 'scenario_content', 'scenario_image_url')
+VALUES ('World War 3 Scenario', 'Noah', 'This is a long scenario description so here is some lorem Ipsum: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Consequat mauris nunc congue nisi vitae suscipit tellus. Platea dictumst quisque sagittis', 'https://worldwar3.com/en/game-overview/');
+
+INSERT INTO 'Scenario' ('scenario_title', 'scenario_author', 'scenario_content', 'scenario_image_url')
+VALUES ('2012', 'Bryan', 'This is a long scenario description so here is some lorem Ipsum: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque.')
+
+INSERT INTO 'Scenario' ('scenario_title', 'scenario_author', 'scenario_content', 'scenario_image_url')
+VALUES ('Global Internet Outage', 'Chris','This is a long scenario description so here is some lorem Ipsum: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. osnfoisdanfoisdfnaoisfnasoidfnasopidfnasoidfnsdi')
+
+-- WW3 plans
+
+INSERT INTO 'Plans' ('plan_title', 'plan_author', 'plan_content') VALUES ('WW3 Survival Plan', 'Bobby', 'This is supposed to be a big string of a plan so here is some Lorem Ipsum:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Consequat mauris nunc congue nisi vitae suscipit tellus. Platea dictumst quisque sagittis purus sit amet volutpat. Faucibus a pellentesque sit amet porttitor eget dolor morbi non. Felis eget nunc lobortis mattis aliquam faucibus purus. Eget lorem dolor sed viverra. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Non arcu risus quis varius quam quisque id diam vel. Vivamus arcu felis bibendum ut tristique. Lectus mauris ultrices eros in cursus. Eget nunc scelerisque viverra mauris in. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Sit amet dictum sit amet. Id leo in vitae turpis massa. Sit amet mattis vulputate enim. Eros in cursus turpis massa tincidunt dui ut ornare. Risus at ultrices mi tempus imperdiet.'
+);
+
+INSERT INTO 'Plans' ('plan_title', 'plan_author', 'plan_content') VALUES ('My WW3 Survival Plan', 'Sean','This is supposed to be a big string of a plan so here is some Lorem Ipsum:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Consequat mauris nunc congue nisi vitae suscipit tellus. Platea dictumst quisque sagittis purus sit amet volutpat. Faucibus a pellentesque sit amet porttitor eget dolor morbi non. Felis eget nunc lobortis mattis aliquam faucibus purus. Eget lorem dolor sed viverra. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Non arcu risus quis varius quam qui')
+
+-- 2012 scenarios
+
+INSERT INTO 'Plans' ('plan_title', 'plan_author', 'plan_content') VALUES ('2012 Survival Guide', 'Brian', 'Description test: SIOiubsfiuasdbfiuasdbfuiasodbfiuasdbfiuasdbfiasuodfbasiuodfbasiuodfbasiudfbiasuodfbasiudofbasdiufbadsuiofbuifasiubdfsuibfsdbuiadfsoibuadfsbuiadfsbuidfsbiudfsbuiadfsiubadfsbiuafbiuafsbiufadsbiuoafssdf')
+
+INSERT INTO 'Plans' ('plan_title', 'plan_author', 'plan_content') VALUES ('The Definitive 2012 Survival Guide', 'Jason', 'I am tired of making descriptions. Even the copy pasting is a hassle since I want something else on my clipboard so this one is going to be short. Hopefully it passes my validation.')
+
+-- Global internet Outage
+
+INSERT INTO 'Plans' ('plan_title', 'plan_author', 'plan_content') VALUES ('A guide to life after the internet', 'Annie', 'Making plan contents sucks. But so does life without the internet. You should probably prevent the world from getting to this stage, but if it does, here is my guide: roll over and die, life is not worth living without the internet')
+
+INSERT INTO 'Plans' ('plan_title', 'plan_author', 'plan_content') VALUES ('Life after internet: A guide on the internet', 'IHateIrony32', 'Congratulations on reading this guide early. I assume the internet is still available if you are reading this guide. If not, this is an archive. If you are a historian, Hello! If not, read on to my very thorough and complete guide to suriving life after the internet:
+
+Go outside, get sunburned, regret, repeat')
+
