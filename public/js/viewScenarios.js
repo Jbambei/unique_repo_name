5b18ada44386fcd822340b6063dc0d24f8602a87
Brian Lefkowitz <brianlefkowitz@Brians-MacBook-Pro.local>
2019-06-27 20:18:31 -0400

    view scenarios js completed

--- public/js/viewScenarios.js
+++ public/js/viewScenarios.js
@@ -1,8 +1,17 @@
 $(function() {
-  $("#viewAllScenarios").on("load", function(event) {
-    $.get("/api/examples", function(data) {
-      console.log("scenarios " + data);
-      for (var i = 0; i < 4; i++) {}
-    });
+  $.get("/api/examples", function(data) {
+    console.log("scenarios " + data);
+    var scenarioTitle = data.scenario_title;
+    var scenarioPicture = data.scenario_image_url;
+    var scenarioDescription = data.scenario_content;
+    var briefDescription = "";
+    for (var i = 0; i < data.length; i++) {
+      briefDescription = scenarioDescription.slice(0, 300);
+    }
+    for (var i = 0; i < 4; i++) {
+      $("#scenarioTitle").text(scenarioTitle);
+      $("#scenarioPicture").attr("src", scenarioPicture);
+      $("#briefDescription").text(briefDescription);
+    }
   });
 });
