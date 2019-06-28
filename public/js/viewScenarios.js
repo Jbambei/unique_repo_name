381ac2aff00614a1ec0b9360338c7d868016164e
Brian Lefkowitz <brianlefkowitz@Brians-MacBook-Pro.local>
2019-06-27 21:32:55 -0400

    server file fixed and script tags added on view.html

--- public/js/viewScenarios.js
+++ public/js/viewScenarios.js
@@ -1,17 +1,23 @@
-$(function() {
-  $.get("/api/examples", function(data) {
-    console.log("scenarios " + data);
-    var scenarioTitle = data.scenario_title;
-    var scenarioPicture = data.scenario_image_url;
-    var scenarioDescription = data.scenario_content;
-    var briefDescription = "";
-    for (var i = 0; i < data.length; i++) {
-      briefDescription = scenarioDescription.slice(0, 300);
-    }
-    for (var i = 0; i < 4; i++) {
-      $("#scenarioTitle").text(scenarioTitle);
-      $("#scenarioPicture").attr("src", scenarioPicture);
-      $("#briefDescription").text(briefDescription);
-    }
-  });
+console.log("connected");
+jQuery(function() {
+//   $.ajax("/api/scenario", {
+//     type: "get"
+//   }).then(function(data) {
+//     console.log("scenarios " + data);
+//     var scenarioTitle = data.scenario_title;
+//     var scenarioPicture = data.scenario_image_url;
+//     var scenarioDescription = data.scenario_content;
+//     var briefDescription = "";
+//     for (var i = 0; i < data.length; i++) {
+//       briefDescription = scenarioDescription.slice(0, 300);
+//     }
+//     for (var i = 0; i < 4; i++) {
+//       $("#scenarioTitle").text(scenarioTitle);
+//       $("#scenarioPicture").attr("src", scenarioPicture);
+//       $("#briefDescription").text(briefDescription);
+//     }
+//   });
+    $.get("/api/scenario", function(data){
+        console.log(data);
+    })
 });
