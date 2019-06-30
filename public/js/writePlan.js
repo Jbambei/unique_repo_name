f2f00bd94111af0fad988ad4a89a4170e7fc5e50
Brian Lefkowitz <brianlefkowitz@Brians-MacBook-Pro.local>
2019-06-29 23:21:32 -0400

    css added in cards

--- public/js/writePlan.js
+++ public/js/writePlan.js
@@ -12,29 +12,38 @@ $(function() {
     $("#fullSceneDescription").text(data.scenario_content);
   });
 
-  $(".newPlan").on("submit", function(event){
-      event.preventDefault();
+  $(".newPlan").on("submit", function(event) {
+    event.preventDefault();
 
-      if(
-          $("#planAuthor").val() !== "" ||
-          $("#planName").val() !== "" ||
-          $("#newPlanStory").val() !== "" 
-      ) {
-          var newPlan = {
-              plan_author: $("#planAuthor").val().trim(),
-              plan_title: $("#planName").val().trim(),
-              plan_content: $("#newPlanStory").val().trim(),
-              ScenarioId: localStorage.id
-          };
-      }
-      console.log(newPlan)
+    if (
+      $("#planAuthor").val() !== "" ||
+      $("#planName").val() !== "" ||
+      $("#newPlanStory").val() !== ""
+    ) {
+      var newPlan = {
+        // eslint-disable-next-line camelcase
+        plan_author: $("#planAuthor")
+          .val()
+          .trim(),
+        // eslint-disable-next-line camelcase
+        plan_title: $("#planName")
+          .val()
+          .trim(),
+        // eslint-disable-next-line camelcase
+        plan_content: $("#newPlanStory")
+          .val()
+          .trim(),
+        ScenarioId: localStorage.id
+      };
+    }
+    console.log(newPlan);
 
-      $.ajax("/api/plans", {
-          type: "POST",
-          data: newPlan
-      }).then(function(){
-          console.log("New Plan Created");
-          location.pathname = "./view";
-      });
+    $.ajax("/api/plans", {
+      type: "POST",
+      data: newPlan
+    }).then(function() {
+      console.log("New Plan Created");
+      location.pathname = "./view";
+    });
   });
 });
